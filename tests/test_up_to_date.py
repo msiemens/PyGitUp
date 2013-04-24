696d0403de4d781fe91b98cb8b83f8e36d2a0063
Markus Siemens <siemens1993@gmail.com>
2013-04-24 12:05:38 +0200

    Added test: up to date

---
+++ tests/test_up_to_date.py
@@ -0,0 +1,48 @@
+# System imports
+import os
+from os.path import join
+
+# 3rd party libs
+from nose.tools import *
+from git import *
+
+# PyGitup imports
+from PyGitUp.git_wrapper import GitError
+from tests import basepath, write_file, init_master, update_file
+
+test_name = 'up-to-date'
+
+repo_path = join(basepath, test_name + os.sep)
+
+
+def setup():
+    master_path, master = init_master(test_name)
+
+    # Prepare master repo
+    master.git.checkout(b=test_name)
+
+    # Clone to test repo
+    path = join(basepath, test_name)
+
+    master.clone(path, b=test_name)
+    repo = Repo(path, odbt=GitCmdObjectDB)
+
+    assert repo.working_dir == path
+
+    # Modify file in master
+    update_file(master, test_name)
+
+    # Update repo
+    repo.remotes.origin.pull()
+
+
+def test_fast_forwarded():
+    """ Run 'git up' with result: fast-forwarded """
+    os.chdir(repo_path)
+
+    from PyGitUp.gitup import GitUp
+    gitup = GitUp()
+    gitup.run(testing=True)
+
+    assert_equal(len(gitup.states), 1)
+    assert_equal(gitup.states[0], 'up to date')
