b98d0864398a81ec56bd19504015466913959c4f
Markus Siemens <siemens1993@gmail.com>
2013-05-07 10:32:50 +0200

    Added test for git-up.fetch.all

---
+++ tests/test_fetchall.py
@@ -0,0 +1,70 @@
+# System imports
+import os
+from os.path import join
+
+# 3rd party libs
+from nose.tools import *
+from git import *
+
+# PyGitup imports
+from PyGitUp.git_wrapper import GitError
+from tests import basepath, write_file, init_master
+
+test_name = 'fetch-all'
+testfile_name = 'file'
+
+repo_path = join(basepath, test_name + os.sep)
+
+
+import contextlib
+@contextlib.contextmanager
+def capture():
+    import sys
+    from cStringIO import StringIO
+    oldout, olderr = sys.stdout, sys.stderr
+    try:
+        out = [StringIO(), StringIO()]
+        sys.stdout, sys.stderr = out
+        yield out
+    finally:
+        sys.stdout,sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
+
+
+def setup():
+    master_path, master = init_master(test_name)
+    master_path2, master2 = init_master(test_name + '2')
+
+    # Prepare master repo
+    master.git.checkout(b=test_name)
+
+    # Clone to test repo
+    path = join(basepath, test_name)
+
+    master.clone(path, b=test_name)
+    repo = Repo(path, odbt=GitCmdObjectDB)
+
+    assert repo.working_dir == path
+
+    # Configure git up
+    repo.git.config('git-up.fetch.all', 'true')
+
+    # Add second master repo to remotes
+    repo.git.remote('add', test_name, master_path2)
+
+
+def test_ahead_of_upstream():
+    """ Run 'git up' with result: ahead of upstream """
+    os.chdir(repo_path)
+
+    from PyGitUp.gitup import GitUp
+    gitup = GitUp()
+
+    with capture() as [stdout, stderr]:
+        gitup.run(testing=True)
+
+    stdout = stdout.getvalue()
+
+    assert_true('origin' in stdout)
+    assert_true(test_name in stdout)
