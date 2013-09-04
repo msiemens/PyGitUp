88486eb4e2e3371da42f032d604490454cd6046b
Markus Siemens <siemens1993@gmail.com>
2013-09-04 19:01:43 +0200

    Fixed test for unstashing error

--- tests/test_unstash_error.py
+++ tests/test_unstash_error.py
@@ -8,10 +8,9 @@ from git import *
 
 # PyGitup imports
 from PyGitUp.git_wrapper import GitError
-from tests import basepath, write_file, init_master, wip
+from tests import basepath, write_file, init_master, wip, testfile_name
 
 test_name = 'unstash_error'
-testfile_name = 'file'
 repo_path = join(basepath, test_name + os.sep)
 
 
@@ -29,32 +28,17 @@ def setup():
 
     assert repo.working_dir == path
 
-    # Create checkpoint
-    repo.git.checkout(b='checkpoint')
-
     # Modify file in master
     master_path_file = join(master_path, testfile_name)
-    write_file(master_path_file , 'line 1\nline 2\ncounter: 2')
+    write_file(master_path_file, 'contents')
     master.index.add([master_path_file])
     master.index.commit(test_name)
 
-    # Modify file in test-3 and commit
+    # Modify file in repo
     path_file = join(path, testfile_name)
-    write_file(path_file , 'line 1\nline 2\ncounter: 3')
-    repo.index.add([path_file])
-    repo.index.commit(test_name)
+    os.unlink(path_file)
 
-    # Modify file in test-3 and stash
-    path_file = join(path, testfile_name)
-    write_file(path_file, 'conflict!')
 
-    repo.git.stash()
-
-    # Setup repositories
-    repo.head.reset('checkpoint', True, True)
-    repo.git.stash('pop')
-
-@wip
 @raises(GitError)
 def test_unstash_error():
     """ Run 'git up' with an unclean unstash """
