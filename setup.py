6f88b37f95de78b86f89ce0af82c84edfff9f6d6
Markus Siemens <siemens1993@gmail.com>
2013-10-08 16:14:49 +0200

    Fixed some problems with package dependencies

--- setup.py
+++ setup.py
@@ -2,36 +2,40 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "git-up",
-    version = "1.1.1",
-    packages = find_packages(),
-    scripts = ['PyGitUp/gitup.py'],
-    install_requires = ['GitPython==0.3.2.RC1', 'colorama==0.2.5', 'termcolor==1.1.0', 'docopt==0.6.1'],
+    name="git-up",
+    version="1.1.1",
+    packages=find_packages(exclude=["tests"]),
+    scripts=['PyGitUp/gitup.py'],
+    install_requires=['GitPython==0.3.2.RC1', 'colorama==0.2.4,==0.2.7',
+                      'termcolor==1.1.0', 'docopt==0.6.1'],
 
     # Tests
     test_suite="nose.collector",
-    tests_require = 'nose',
+    tests_require='nose',
 
-    entry_points = {
+    # Executable
+    entry_points={
         'console_scripts': [
             'git-up = gitup:run'
         ]
     },
 
-    package_data = {
-        'PyGitUp': ['check-bundler.rb']
+    # Additional data
+    package_data={
+        'PyGitUp': ['check-bundler.rb'],
+        '': ['README.rst', 'LICENCE']
     },
 
-    zip_safe = False,
+    zip_safe=False,
 
-    # metadata for upload to PyPI
-    author = "Markus Siemens",
-    author_email = "markus@m-siemens.de",
-    description = "A python implementation of 'git up'",
-    license = "MIT",
-    keywords = "git git-up",
-    url = "https://github.com/msiemens/PyGitUp",
-    classifiers  = [
+    # Metadata
+    author="Markus Siemens",
+    author_email="markus@m-siemens.de",
+    description="A python implementation of 'git up'",
+    license="MIT",
+    keywords="git git-up",
+    url="https://github.com/msiemens/PyGitUp",
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
@@ -40,6 +44,5 @@ setup(
         "Topic :: Utilities"
     ],
 
-    long_description = open('README.rst').read()
-    # could also include download_url etc.
+    long_description=open('README.rst').read()
 )
