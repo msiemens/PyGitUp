ccd10466fc73ef1f8e14e67d8a09db5a66d3cb2e
valentijn <valentijn.scholten@isaac.nl>
2014-03-19 17:33:22 +0100

    Fix windows log hook issues with single/dboule quotes and variable expansion.

--- PyGitUp/gitup.py
+++ PyGitUp/gitup.py
@@ -314,7 +314,9 @@ class GitUp(object):
                 # Prepare log_hook
                 log_hook = re.sub(r'\$(\d+)', r'%\1', log_hook)
                 log_hook = re.sub(r'; ?', r'\n', log_hook)
-
+                log_hook = re.sub(r'\'', '"', log_hook)
+                log_hook = re.sub(r'\%', '%%', log_hook)
+				
                 # Write log_hook to an temporary file and get it's path
                 with NamedTemporaryFile(
                         prefix='PyGitUp.', suffix='.bat', delete=False
