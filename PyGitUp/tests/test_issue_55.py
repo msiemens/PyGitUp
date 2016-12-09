3eab6ae41633fd7b0d33ac559ecc4372c6f2b081
Markus Siemens <siemens1993@gmail.com>
2016-12-09 13:03:07 +0100

    Add test for #55

---
+++ PyGitUp/tests/test_issue_55.py
@@ -0,0 +1,43 @@
+# System imports
+import os
+from os.path import join
+
+from git import *
+from nose.tools import *
+
+from PyGitUp.tests import basepath, init_master, update_file
+
+test_name = 'issue-55'
+repo_path = join(basepath, test_name + os.sep)
+
+
+def setup():
+    master_path, master = init_master(test_name)
+
+    branch_name = 'feature/#11772-replace-api-url'
+
+    # Prepare master repo
+    master.git.checkout(b=branch_name)
+
+    # Clone to test repo
+    path = join(basepath, test_name)
+
+    master.clone(path, b=branch_name)
+    repo = Repo(path, odbt=GitCmdObjectDB)
+
+    assert repo.working_dir == path
+
+    # Modify file in master
+    update_file(master, test_name)
+
+
+def test_issue_55():
+    """ Regression test for #55 """
+    os.chdir(repo_path)
+
+    from PyGitUp.gitup import GitUp
+    gitup = GitUp(testing=True)
+    gitup.run()
+
+    assert_equal(len(gitup.states), 1)
+    assert_equal(gitup.states[0], 'fast-forwarding')
