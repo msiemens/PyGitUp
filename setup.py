4dc129bb801716f6ea49201b6e839a77de04e96a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-24 20:45:37 +0200

    Drop Python 3.4
    This environment is no longer available for testing in Azure and is also
    EOL.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license="MIT",
     keywords="git git-up",
     url="https://github.com/msiemens/PyGitUp",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
@@ -44,7 +44,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Version Control",
